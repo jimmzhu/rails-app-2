744b90da3968dc6f91cbb6dce67f10872e6f2e63
Jimmy Zhu <jimmzhu@gmail.com>
2014-03-05 17:54:19 -0800

    Completed static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PrimeSampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+
+  get 'static_pages' => 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
