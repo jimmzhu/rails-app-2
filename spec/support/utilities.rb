4b19008de5dee7afa0fff8f9e4eb980ef6b900a3
Jimmy Zhu <jimmzhu@gmail.com>
2014-03-07 15:22:04 -0800

    Spec enhancements

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,9 @@
-def full_title(page_title = nil)
-    base_title = 'Super Duper Rails Sample App'
-    if page_title.blank?
-        base_title
-    else
-        "#{base_title} | #{page_title}"
-    end
-end
+include ApplicationHelper
+#def full_title(page_title = nil)
+#    base_title = 'Super Duper Rails Sample App'
+#    if page_title.blank?
+#        base_title
+#    else
+#        "#{base_title} | #{page_title}"
+#    end
+#end
